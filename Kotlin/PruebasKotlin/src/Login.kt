8fb4984dd43bd6e17d195fedb07ed7f343bff67e
OscarLarios7a <javier.atoaxaca@gmail.com>
2020-10-20 20:53:44 -0500

    Cargando la App de TestDesign

--- Kotlin/PruebasKotlin/src/Login.kt
+++ Kotlin/PruebasKotlin/src/Login.kt
@@ -1,21 +1,23 @@
 import java.util.*
 fun main() {
+
     val scan = Scanner(System.`in`)
     var  passwordString: String
     var flagInt: Int=1
 
   while(flagInt<=3){
-      println("No. Intento:$flagInt")
+
+      println("No. Intento: $flagInt ")
       print("Ingresa la Contraseña: ")
       passwordString=scan.nextLine()
+
       if (passwordString == "Larios"){
-          println("Contraseña Correcta")
-          println("Acceso Concedido")
+          println("Contraseña Correcta ")
+          println("Acceso Concedido ")
           break
-      }else{
-          println("Contraseña Incorrecta")
+      } else{
+          println("Contraseña Incorrecta ")
           ++flagInt
       }
-
   }
 }
