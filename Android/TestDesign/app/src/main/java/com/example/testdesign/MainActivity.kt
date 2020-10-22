6fe6fba5068c8c189535c4f5e3907462805fdc16
OscarLarios7a <javier.atoaxaca@gmail.com>
2020-10-21 22:43:01 -0500

    Actualizacion de Proyecto

--- Android/TestDesign/app/src/main/java/com/example/testdesign/MainActivity.kt
+++ Android/TestDesign/app/src/main/java/com/example/testdesign/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.testdesign
 
+
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+
+
