c9ec88f6f5070952d2d1559b59e8f88e515ecf03
OscarLarios7a <javier.atoaxaca@gmail.com>
2020-11-24 20:07:16 -0600

    [update] agregando fragamento central, solo falta ahora el segundo fragmento

--- Android/RecyclerFragment/app/src/main/java/com/example/recyclerfragment/MainActivity.kt
+++ Android/RecyclerFragment/app/src/main/java/com/example/recyclerfragment/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.recyclerfragment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerfragment.adapter.ContactAdapter
+import com.example.recyclerfragment.fragment.DescriptionFragment
+import com.example.recyclerfragment.model.Contact
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    val transition=supportFragmentManager.beginTransaction()
+        transition.replace(R.id.container,DescriptionFragment())
+        transition.addToBackStack(null)
+        transition.commit()
+
     }
+
+
 }
--- Android/RecyclerFragment/app/src/main/java/com/example/recyclerfragment/fragment/DescriptionFragment.kt
+++ Android/RecyclerFragment/app/src/main/java/com/example/recyclerfragment/fragment/DescriptionFragment.kt
@@ -5,20 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerfragment.R
+import com.example.recyclerfragment.adapter.ContactAdapter
+import com.example.recyclerfragment.adapter.listeners.ContactListener
+import com.example.recyclerfragment.model.Contact
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_description.*
 
 
-class DescriptionFragment : Fragment() {
+class DescriptionFragment : Fragment(), ContactListener {
 
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_description, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val contacts=ArrayList<Contact>()
+        contacts.add(Contact("Rogelio Cardenas ",9512746599,"Alumno de ApprenDev", R.drawable.ic_icons8male))
+        contacts.add(Contact("Javier Larios",9512341199,"Alumno de ApprenDev", R.drawable.ic_icons8male))
+        contacts.add(Contact("Carolina Jacome",9512115199,"Alumno sin acceso", R.drawable.ic_icons8businesswoman))
+
+        val adapter= ContactAdapter(contacts,this)
+        val dividerItem= DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        rvContacts.addItemDecoration(dividerItem)
+        rvContacts.layoutManager= LinearLayoutManager(requireContext())
+        rvContacts.setHasFixedSize(true)
+        rvContacts.adapter=adapter
+
+    }
+
+    override fun clickContact(contact: Contact) {
+        Toast.makeText(requireContext(),"${contact.phone}",Toast.LENGTH_SHORT).show()
+
     }
 
 
